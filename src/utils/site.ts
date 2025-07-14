8b37b67e80fae3918a26009572ba635cecf9a0e5
sesukneksa <sesukneksa@gmail.com>
2025-07-15 01:12:06 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Indo Rame';
-export const desk = 'Video Bokep Indo Jepang Jav Hijab Tante Viral Terbaru Bocil Ngentot Jilbab Smp Skandal';
-export const url = 'https://indorame.pages.dev';
-export const terbit = '2025-06-27T00:01:03Z';
